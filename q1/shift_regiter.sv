c01e5b65e9326d792a21c0cb773a759a5e438987
rT505 <rizah@tutanota.com>
2025-09-17 09:34:38 +0200

    Update shift_regiter.sv

--- q1/shift_regiter.sv
+++ q1/shift_regiter.sv
@@ -7,7 +7,19 @@ module shift_register #(parameter N=4)
                        input logic [N-1:0] parallel_in,
                        output logic [N-1:0] parallel_out,
                        output logic serial_out);
+  
+logic [N-1:0] LFSR_ff;
 
-//complete here
-
+always_ff @(posedge clk or negedge rst_n) begin
+    if (~rst_n) begin
+        LFSR_ff <= '0;
+    end else if (load_enable) begin
+        if(serial_parallel)
+        LFSR_ff <= parallel_in;
+        else LFSR_ff <= {serial_in, LFSR_ff[N-1:1]};
+    end
+end
+assign parallel_out = LFSR_ff;
+assign serial_out = LFSR_ff[N-1];
 endmodule
+
