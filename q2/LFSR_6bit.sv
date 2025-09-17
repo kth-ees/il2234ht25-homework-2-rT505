2ffa67a68d5b8c1bfa1fb7d5388c978b6c5617ce
rT505 <rizah@tutanota.com>
2025-09-17 09:49:25 +0200

    Update LFSR_6bit.sv

--- q2/LFSR_6bit.sv
+++ q2/LFSR_6bit.sv
@@ -4,7 +4,17 @@ module LFSR_6bit (
   input  logic [5:0] parallel_in,
   output logic [5:0] parallel_out
 );
-  // …
-  // Add your description here
-  // …
+ logic feedback;
+
+always_ff @(posedge clk or negedge rst_n) begin
+    if (~rst_n)
+        parallel_out <= 6'b000001;
+        else if (!sel)
+            out <= parallel_in;
+            else begin
+                feedback = out[5] ^ out[2] ^ out[0];
+                out <= {feedback, out[5:1]};
+            end
+end
 endmodule
+
