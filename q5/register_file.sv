b38bc4e26748b35091fa104abc6671f5addbc721
rT505 <rizah@tutanota.com>
2025-09-17 10:02:48 +0200

    Update register_file.sv

--- q5/register_file.sv
+++ q5/register_file.sv
@@ -8,6 +8,14 @@ module registerfile (input logic clk,
                      output logic [7:0] data_out1,
                      output logic [7:0] data_out2
                      );
-    // complete here
+logic [7:0] register [15:0];
 
+always_ff @(posedge clk or negedge rst_n) begin
+    if (~rst_n)
+        for (int i = 0; i < 16; i++) register[i] = 8'b0;
+        else if (write_en)
+          register[write_addr] <= data_in; 
+end
+assign data_out1 = register[read_addr1];
+assign data_out2 = register[read_addr2];
 endmodule
